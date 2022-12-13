70490a719aca3723c9d738d9fc93b0969c042257
Nontaraporn <tatami.non@gmail.com>
2022-12-14 01:06:46 +0700

    Success 90% :+1:
    - No link some point
    - Not OO Programing

--- src/app/body/use-cases/use-cases.component.ts
+++ src/app/body/use-cases/use-cases.component.ts
@@ -7,6 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UseCasesComponent implements OnInit {
 
+  public useCases = [
+    {
+      url:'https://drive.google.com/file/d/1IAl9aZDaOxgJPHZRBgJkLah1BqPAkvEJ/preview',
+      title:'Marketing Video'
+    },
+    {
+      url:'data/item02.jpg',
+      title:'Avatar Conversations'
+    },
+    {
+      url:'data/item03.jpg',
+      title:'Customer Communications'
+    },
+    {
+      url:'data/item04.jpg',
+      title:'Entertainment Content'
+    },
+    {
+      url:'data/item05.jpg',
+      title:'Educational Content'
+    },
+  ]
+  public content = [
+    {
+      title:'Reporter',
+      detail:"เปลี่ยนเสียง content วิดิโอที่เป็นภาษาอังกฤษอยู่แล้ว ให้กลายเป็นภาษาไทย (หรือจากไทยเป็นอังกฤษ)",
+      url:"data/content1.svg",
+    },
+    {
+      title:'Instructor',
+      detail:"Machine translation เพื่อแปลภาษาองกฤษเป็นภาษาไทยและใช้ Text to speech เปลี่ยนข้อความภาษาไทยให้เป็นเสียงภาษาไทย",
+      url:"data/content2.svg",
+    },
+    {
+      title:'Presentation',
+      detail:"แปลงคลิปโดยนำเสียงและวิดิโอผ่านเทคโนโลยี Speech to video ให้มีเสียงและขยับปากอย่างเป็นธรรมชาติ",
+      url:"data/content3.svg",
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +11,29 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // scroll chang style nav
+    let element = document.querySelector('.navbar') as HTMLElement;
+    // console.log("current",this.location.path().slice(1));
+    if (window.pageYOffset > 40 ) {
+      element.classList.add('sticky');
+    }else {
+      element.classList.remove('sticky');
+    }
+  }
+  // $(window).scroll(function(){
+	// 	if(this.scrollY > 20){
+	// 		('.navbar').addClass("sticky");
+	// 	}else{
+	// 		('.navbar').removeClass("sticky");
+	// 	}
+	// });
+
+	// slide up
+	// $('.scroll-up-btn').click(function(){
+	// 	$('html').animate({scrollTop: 0});
+	// })
 
 }
